5efc79d3d64a377e48b50f411fb888aab18f876c
root <root@Dacosta.localdomain>
2021-10-27 17:43:56 -0500

    models_user

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
+from uuid import UUID
+
+from pydantic import BaseModel
+from pydantic import EmailStr
+from pydantic import Field
+
 from fastapi import FastAPI
 
+
 app = FastAPI()
+
+#model
+class User(BaseModel):
+    user_id = UUID
+    email : EmailStr = Field(...)
+    
+
+class Tweet(BaseModel):
+    pass
+
+
+
 
 @app.get("/")
 def home():
